252396d2f7e5a4a8abe25a4fad0fff836869e597
imjohnnyy <ldong4056@gmail.com>
2023-06-27 16:08:02 +1200

    added login button

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -7,10 +7,8 @@ import SponsorItem from "../components/SponsorItem";
 const Landing = () => {
   return (
     <div className={"bg-lightergray h-screen"} id="home">
-
       {/* HEADER */}
       <header className="flex items-center justify-between w-4/5 p-6 mx-auto">
-
         {/* LOGO */}
         <div className="flex items-center space-x-4">
           <img src={Logo} alt="Logo" className="w-[175px] h-[60px]" />
@@ -33,13 +31,12 @@ const Landing = () => {
 
         {/* TEMPORARY GET STARTED TEXT OR LIGHT/DARK TOGGLE */}
         <div>
-            <a href="#getstarted" className="text-lg font-medium text-gray hover:text-sky-500"> Get started </a>
+            <a href="#login" className="px-4 py-1 text-lg font-medium text-white rounded-full cursor-pointer bg-gray hover:bg-blue-700"> Login</a>
         </div>
 
       </header>
 
       {/* BODY */}
-
       <body className="flex items-center justify-between w-4/5 p-12 mx-auto ">
         <div className="ml-[10%] mt-[-100px]">
           <h1 className={"w-auto item-start text-5xl text-left font-bold text-gray mr-12"}>Job Tracking Made Easy</h1>
@@ -52,15 +49,15 @@ const Landing = () => {
 
 
         <div className={"flex items-center justify-center mt-[-100px]"}>
-          <img className={"mr-[10%] w-[2050px] h-[750px] static"} src={Worker} alt="worker"></img>
+          <img className={"mr-[10%] w-[2050px] h-[650px] static"} src={Worker} alt="worker"></img>
         </div>
       </body>
 
       {/* FOOTER */}
       {/* ADD EDUCATION INSTITUTES LOGOS HERE*/}
       <footer className="bg-lightergray ">
-        <h2 className="mb-4 text-3xl font-bold">Our Sponsors</h2>
-        <div className="flex items-center justify-between w-4/5 p-6 mx-auto">
+        <h2 className="mb-6 text-3xl font-bold">Our Sponsors</h2>
+        <div className="flex items-center justify-between w-3/5 p-2 mx-auto">
           {SponsorList.map((sponsor) => {
               return (
                 <SponsorItem image={sponsor.image} />
