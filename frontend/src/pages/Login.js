ba89a5a62cdacea25f977b410a5004b2d2e7223c
imjohnnyy <ldong4056@gmail.com>
2023-07-02 21:53:31 +1200

    added background color changes for contrast

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -11,8 +11,8 @@ const Login = () => {
   };
 
   return (
-    <div className="flex items-center justify-center h-screen">
-      <form className="w-full max-w-sm px-6 py-8 rounded-lg shadow-[0_0px_10px_rgba(0,0,0,0.25)]" onSubmit={handleSubmit}>
+    <div className="flex items-center justify-center h-screen bg-lightergray">
+      <form className="w-full max-w-sm px-6 py-8 rounded-lg shadow-[0_0px_10px_rgba(0,0,0,0.25)] bg-white" onSubmit={handleSubmit}>
 
         {/* Logo */}
         <div className={"flex items-center justify-center"}>
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -12,8 +12,8 @@ const Register = () => {
   };
 
   return (
-    <div className="flex items-center justify-center h-screen">
-      <form className="w-full max-w-sm px-6 py-8 rounded-lg shadow-[0_0px_10px_rgba(0,0,0,0.25)]" onSubmit={handleSubmit}>
+    <div className="flex items-center justify-center h-screen bg-lightergray">
+      <form className="w-full max-w-sm px-6 py-8 rounded-lg shadow-[0_0px_10px_rgba(0,0,0,0.25)] bg-white" onSubmit={handleSubmit}>
 
         {/* Logo */}
         <div className={"flex items-center justify-center"}>
