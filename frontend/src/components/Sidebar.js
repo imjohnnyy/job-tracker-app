d18c3d688ba5a8a3c282970b45e17d8bf707f5d9
imjohnnyy <ldong4056@gmail.com>
2023-07-07 20:36:37 +1200

    adjusted navlink text color of sidebar

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -8,8 +8,10 @@ import PostAddIcon from '@mui/icons-material/PostAdd';
 const Sidebar = () => {
 
   return (
+
     // Remove this top div 
     <div className="flex h-screen bg-lightergray">   
+    
       <div className="flex flex-col w-64 px-8 py-4 bg-gray">
         {/* Logo */}
         <div className="flex justify-between">
@@ -20,17 +22,17 @@ const Sidebar = () => {
         <nav className="mt-[5rem]">
           <ul className="space-y-3">
             <li>
-              <a href="#dashboard" className="flex items-center justify-start py-2 text-lg font-semibold rounded-sm text-lightgray hover:text-white hover:bg-slate-500">
+              <a href="#dashboard" className="flex items-center justify-start py-2 text-lg font-semibold rounded-sm text-zinc-300 hover:text-white hover:bg-slate-500">
               <DashboardIcon className="my-1 ml-[10px] mr-[8px]" style={{ fontSize: '150%' }} /> Dashboard
               </a>
             </li>
             <li>
-              <a href="#about" className="flex items-center justify-start py-2 text-lg font-semibold rounded-sm text-lightgray hover:text-white hover:bg-slate-500">
+              <a href="#about" className="flex items-center justify-start py-2 text-lg font-semibold rounded-sm text-zinc-300 hover:text-white hover:bg-slate-500">
               <PostAddIcon className="my-1 ml-[9px] mr-[5px]" style={{ fontSize: '180%' }} /> Add Jobs
               </a>
             </li>
             <li>
-              <a href="#about" className="flex items-center justify-start py-2 text-lg font-semibold rounded-sm text-lightgray hover:text-white hover:bg-slate-500">
+              <a href="#about" className="flex items-center justify-start py-2 text-lg font-semibold rounded-sm text-zinc-300 hover:text-white hover:bg-slate-500">
                 <AccountBoxIcon className="my-1 ml-[10px] mr-[8px]" style={{ fontSize: '150%' }}/> My Profile
               </a>
             </li>
