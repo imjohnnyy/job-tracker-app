8947166dfcc2e5afc578fe3a639f9674ef142071
imjohnnyy <ldong4056@gmail.com>
2023-07-08 22:18:41 +1200

    added side header to dashboard page

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -3,8 +3,15 @@ import Sidebar from "../components/Sidebar"
 const Dashboard = () => {
   return (
     <div className="flex h-screen bg-lightergray">   
-        {/* Sidebar */}
-        <Sidebar />
+      {/* Sidebar */}
+      <Sidebar />
+      
+      {/* Right Side Header */}
+      <div className="flex-1 p-0">
+        <h1 className="flex items-start p-8 text-3xl font-bold bg-white ">Dashboard</h1>
+      </div>
+
+      {/* Content */}
 
     </div>
   )
