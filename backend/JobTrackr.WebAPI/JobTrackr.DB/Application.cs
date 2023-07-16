e2da45e72ec42d739a459701480fb27ce037b733
imjohnnyy <ldong4056@gmail.com>
2023-07-16 17:37:38 +1200

    Setting up backend, installed EntityFramework and SQL Server packages. Added Application model.

---
+++ backend/JobTrackr.WebAPI/JobTrackr.DB/Application.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+// This file defines the Application model for tracking job applications.
+
+namespace JobTrackr.DB
+{
+    public class Application
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Company { get; set; }
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string JobType { get; set; }
+
+        public string JobStatus { get; set; }
+    }
+}
