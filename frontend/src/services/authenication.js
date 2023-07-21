d9ac965a5b29f97f65600be5a701d4345c5e17f3
imjohnnyy <ldong4056@gmail.com>
2023-07-21 18:38:27 +1200

    added authentication service

---
+++ frontend/src/services/authenication.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { userAuthenticated } from "../redux/authenticationSlice";
+
+
+const axiosInstance = axios.create({
+    baseURL: `${process.env.REACT_APP_BASE_URL}/Authenication`,
+});
+
+
+export const SignUp = async (dispatch, userCredentials) => {
+    try {
+        // Makes a POST request to the "/signup" endpoint with the provided user credentials
+        const { data } = await axiosInstance.post("/signup", userCredentials);
+
+        // If the request is successful, dispatch the userAuthenticated action with the returned data
+        dispatch(userAuthenticated(data));
+    } catch {
+        console.log("Error");
+    }
+};
+
+export const SignIn = async (dispatch, userCredentials) => {
+    try {
+        // Makes a POST request to the "/signin" endpoint with the provided user credentials
+        const { data } = await axiosInstance.post("/signin", userCredentials);
+
+        // If the request is successful, dispatch the userAuthenticated action with the returned data
+        dispatch(userAuthenticated(data));
+    } catch {
+        console.log("Error");
+    }
+};
