a2acc062b94d01bf685c5f36789b04be3aa21ec0
imjohnnyy <ldong4056@gmail.com>
2023-07-26 19:58:19 +1200

    refactored dashboard and add applications pages

--- frontend/src/pages/AddApplications.js
+++ frontend/src/pages/AddApplications.js
@@ -1,11 +1,20 @@
 import Sidebar from "../components/Sidebar";
 import ApplicationForm from "../components/ApplicationForm";
 import ApplicationList from "../components/ApplicationList";
-import HamburgerNav from "../components/HamburgerNav";
-
+import LogoutIcon from '@mui/icons-material/Logout';
+import HamburgerNav from "../components/HamburgerNavbar";
+import { useDispatch } from "react-redux"
+import { userLoggedOut } from "../redux/authenticationSlice"
 
 const AddApplications = () => {
+  const dispatch = useDispatch()
 
+  const handleLogOut = () => {
+    dispatch(userLoggedOut())
+    console.log("User has logged out");
+  }
+
+ 
   return (
     <div class="flex bg-lightergray min-h-[150vh]">
       {/* Sidebar */}
@@ -14,12 +23,21 @@ const AddApplications = () => {
       <div className="flex flex-col flex-1">
         {/* Right Side Header */}
         <header className="relative flex items-center justify-between p-8 bg-white shadow-md">
+
+          {/* Hamburger Menu for smaller devices */}
+          <HamburgerNav />
+
+          {/* Header Title */}
           <div className="flex items-start text-3xl font-bold">
-            <h1 className="max-md:items-center max-md:justify-center max-md:ml-8">
-              Add Job Applications
-            </h1>
+              <h1 className="max-md:items-center max-md:justify-center max-md:ml-8">
+                My Job Applications
+              </h1>
           </div>
-         <HamburgerNav />
+
+          {/* Sign Out button and Logo */}
+          <p className="block py-3 pl-0 cursor-pointer max-md:w-1/3 max-md:mr-[-2rem]" onClick={handleLogOut}>
+            <LogoutIcon style={{ fontSize: 25 }} /> Sign Out
+          </p>
         </header>
 
         {/* Job Application Form */}
--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -5,26 +5,17 @@ import { userLoggedOut } from "../redux/authenticationSlice"
 import { Pie } from 'react-chartjs-2';
 import 'chart.js/auto';
 import LogoutIcon from '@mui/icons-material/Logout';
-import MenuIcon from "@mui/icons-material/Menu";
-import CloseIcon from "@mui/icons-material/Close";
-import { useNavigate } from 'react-router-dom';
+import HamburgerNav from "../components/HamburgerNavbar";
+
 
 const Dashboard = () => {
-  const [isExpandNavbar, setIsExpandNavbar] = useState(false);
+
   const dispatch = useDispatch()
 
   const handleLogOut = () => {
     dispatch(userLoggedOut())
     console.log("User has logged out");
   }
-
-  // Navbar toggle function
-  const handleNav = () => {
-    setIsExpandNavbar(!isExpandNavbar);
-  };
-
-  // Navigation using React Router
-  const navigate = useNavigate();
 
 
   // Data for the pie chart
@@ -43,12 +34,17 @@ const Dashboard = () => {
 
   return (
     <div className="flex h-screen bg-lightergray">   
-      {/* Sidebar */}
+      {/* Side navbar */}
       <Sidebar />
       
       <div className="flex flex-col flex-1">
         {/* Right Side Header */}
         <header className="relative flex items-center justify-between p-8 bg-white shadow-md">
+  
+          {/* Hamburger Menu for smaller devices */}
+          <HamburgerNav />
+          
+          {/* Header Title */}
           <div className="flex items-start text-3xl font-bold">
             <h1 className="max-md:items-center max-md:justify-center max-md:ml-8">
               Dashboard
@@ -56,37 +52,7 @@ const Dashboard = () => {
             
           </div>
 
-          {/* Navigation option for smaller devices (Hamburger menu navbar) */}
-          <div className="block px-4 py-3 md:hidden" onClick={handleNav}>
-            <MenuIcon style={{ fontSize: 35 }} />
-            <ul
-              className={
-                isExpandNavbar
-                  ? "fixed left-0 top-0 w-[100%] h-full bg-white ease-in-out duration-500 flex flex-col items-center justify-center text-2xl text-gray font-semibold"
-                  : "ease-in-out duration-500 fixed left-[-100%]"
-              }
-            >
-              <div className={"flex ml-auto mr-12 mb-20 mt-[-120px]"}>
-                <CloseIcon style={{ fontSize: 35 }} />
-              </div>
-              <li className={"py-5"}>
-                <h1 onClick={() => navigate('/')} className={"hover:text-sky-500"}>
-                  Dashboard
-                </h1>
-              </li>
-              <li className={"py-5"}>
-                <h1 href="#add-applications" onClick={() => navigate('/add-applications')} className={"hover:text-sky-500"}>
-                  Add Jobs
-                </h1>
-              </li>
-              <li className={"py-5"}>
-                <h1 href="#myprofile" className={"hover:text-sky-500"}>
-                  My Profile
-                </h1>
-              </li>
-            </ul>
-          </div>
-
+          {/* Sign Out button and Logo */}
           <p className="block px-4 py-3 cursor-pointer" onClick={handleLogOut}>
             <LogoutIcon style={{ fontSize: 25 }} /> Sign Out
           </p>
@@ -96,6 +62,7 @@ const Dashboard = () => {
         <div className="p-8 mx-auto w-96 h-96">
           <Pie data={data} />
         </div>
+
       </div>
 
 
