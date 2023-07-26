ed268b6b91485072e293c07d4d256c5c669a3275
imjohnnyy <ldong4056@gmail.com>
2023-07-26 18:54:31 +1200

    added dummy data for pie chart

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,6 +1,8 @@
 import Sidebar from "../components/Sidebar"
 import { useDispatch } from "react-redux"
 import { userLoggedOut } from "../redux/authenticationSlice"
+import { Pie } from 'react-chartjs-2';
+import 'chart.js/auto';
 
 const Dashboard = () => {
   const dispatch = useDispatch()
@@ -10,6 +12,20 @@ const Dashboard = () => {
     console.log("User has logged out");
   }
   
+  // Data for the pie chart
+  const data = {
+    labels: ['Ongoing', 'Accepted', 'Rejected', 'Declined'],
+    datasets: [{
+      data: [3, 1, 5, 2],
+      backgroundColor: [
+        '#FCF55F', // Yellow for Ongoing status
+        '#4F7942', // Green for Accepted status
+        '#EE4B2B', // Red for Rejected status
+        '#0096FF', // Blue for Declined status
+      ]
+    }]
+  };
+
   return (
     <div className="flex h-screen bg-lightergray">   
       {/* Sidebar */}
@@ -22,7 +38,9 @@ const Dashboard = () => {
       </div>
 
       {/* Content */}
-     
+      <div class="max-w-[45rem] h-[45rem] mx-auto text-center">
+        <Pie data={data} />
+      </div>
     </div>
   )
 }
