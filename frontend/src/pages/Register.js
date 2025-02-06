415ded6bf0707dce0875893e373ef055ba87882d
Johnny Dong <johnny.dong@up.education>
2025-02-07 12:55:31 +1300

    removed required validation conditions from User Service , but set them to be required from Front End instead

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -58,6 +58,7 @@ const Register = () => {
             className="w-full px-3 py-2 mb-2 leading-tight border rounded border-zinc-300 text-gray focus:outline-none focus:shadow-outline bg-lightergray"
             value={username}
             onChange={(e) => setUserName(e.target.value)}
+            required
           />
         </div>
         <div className="mb-4">
@@ -73,6 +74,7 @@ const Register = () => {
             className="w-full px-3 py-2 mb-2 leading-tight border rounded border-zinc-300 text-gray focus:outline-none focus:shadow-outline bg-lightergray"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            required
           />
         </div>
         <div className="mb-4">
@@ -118,6 +120,7 @@ const Register = () => {
             className="w-full px-3 py-2 mb-2 leading-tight border rounded border-zinc-300 text-gray focus:outline-none focus:shadow-outline bg-lightergray"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            required
           />
         </div>
 
