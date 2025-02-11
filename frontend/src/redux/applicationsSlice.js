898e12356290463b26cd28e36d0b8250f5cf9593
Johnny Dong <johnny.dong@up.education>
2025-02-11 18:23:31 +1300

    Update edit and delete application reducers

--- frontend/src/redux/applicationsSlice.js
+++ frontend/src/redux/applicationsSlice.js
@@ -1,41 +1,51 @@
-import { createSlice, createAction } from '@reduxjs/toolkit';
+import { createSlice, createAction } from "@reduxjs/toolkit";
 
-export const setApplicationsError = createAction('setApplicationsError');
-export const newApplicationError = createAction('newApplicationError');
-export const editApplicationError = createAction('newEditApplicationError');
-export const deleteApplicationError = createAction('deleteApplicationError');  
+export const setApplicationsError = createAction("setApplicationsError");
+export const newApplicationError = createAction("newApplicationError");
+export const editApplicationError = createAction("newEditApplicationError");
+export const deleteApplicationError = createAction("deleteApplicationError");
 
 export const applicationsSlice = createSlice({
-    name: 'applications',
-    initialState: {
-        applications: [], // Initial state with an empty array for job applications
+  name: "applications",
+  initialState: {
+    applications: [], // Initial state with an empty array for job applications
+  },
+  reducers: {
+    // Action: Set the job applications array to a new array
+    setApplications: (state, action) => {
+      return { ...state, applications: [...action.payload] };
     },
-    reducers: {
-        // Action: Set the job applications array to a new array
-        setApplications: (state, action) => {
-            return { ...state, applications: [...action.payload] };
-        },
-        // Action: Add a new job application to the applications array
-        newApplication: (state, action) => {
-            return {...state, applications: [...state.applications, action.payload]};
-        },
-        // Action: Edit an existing job application in the applications array
-        editApplication: (state, action) => {
-            return {...state, applications: [...state.applications.map(application => 
-                application.id === action.payload.id ? action.payload : application
-            )]};
-        },
-        // Action: Delete an job application from the applications array
-        deleteApplication: (state, action) => { 
-            return {...state, applications: [...state.applications.filter(application => 
-                application.id !== action.payload
-            )]};
+    // Action: Add a new job application to the applications array
+    newApplication: (state, action) => {
+      return {...state, applications: [...state.applications, action.payload]};
+    },
+    // Action: Edit an existing job application in the applications array
+    editApplication: (state, action) => {
+      const applications = state.applications.map((application) => {
+        if (application.id === action.payload.id) {
+          application = action.payload;
         }
-    }
+        return application;
+      });
+      return { ...state, applications: [...applications] };
+    },
+    // Action: Delete an job application from the applications array
+    deleteApplication: (state, action) => {
+      const applications = state.applications.filter(
+        (application) => application.id !== action.payload.id
+      );
+      return { ...state, applications: [...applications] };
+    },
+  },
 });
 
 // Export the action creators
-export const { setApplications, newApplication, editApplication, deleteApplication } = applicationsSlice.actions;
+export const {
+  setApplications,
+  newApplication,
+  editApplication,
+  deleteApplication,
+} = applicationsSlice.actions;
 
 // Export the reducer function
 export default applicationsSlice.reducer;
